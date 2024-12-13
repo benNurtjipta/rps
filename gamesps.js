40184c1878bd2b7f65fcee3409b4d069b4420e58
benNurtjipta <ben@nurtjipta.de>
2024-12-13 14:39:27 +0100

    counter added

--- gamesps.js
+++ gamesps.js
@@ -1,4 +1,7 @@
 let userChoice;
+let winCount = 0;
+let lossCount = 0;
+let drawCount = 0;
 
 function winner(userChoice) {
   let randomNum = Math.floor(Math.random() * 3 + 1);
@@ -18,6 +21,7 @@ function winner(userChoice) {
     //   computerDraw.charAt(0).toUpperCase() + computerDraw.slice(1)
     // }, you draw`;
     result = "draw";
+    drawCount++;
   } else if (
     (userChoice === "rock" && computerDraw === "scissors") ||
     (userChoice === "scissors" && computerDraw === "paper") ||
@@ -27,17 +31,21 @@ function winner(userChoice) {
     //   computerDraw.charAt(0).toUpperCase() + computerDraw.slice(1)
     // }, you win`;
     result = "win";
+    winCount++;
   } else {
     // return `Computer: ${
     //   computerDraw.charAt(0).toUpperCase() + computerDraw.slice(1)
     // }, you lose`;
     result = "lose";
+    lossCount++;
   }
   return [computerDraw, result];
 }
 
 const pixelArtContainer = document.querySelector(".pixel-art");
 const outputElement = document.getElementById("output");
+const scoreElement = document.getElementById("score");
+const resetButton = document.getElementById("resetButton");
 
 pixelArtContainer.addEventListener("click", function (event) {
   const target = event.target;
@@ -49,7 +57,20 @@ pixelArtContainer.addEventListener("click", function (event) {
     }, Me: ${
       computerDraw.charAt(0).toUpperCase() + computerDraw.slice(1)
     }. You ${result}!`;
+    updateScore();
   }
+});
+
+function updateScore() {
+  scoreElement.textContent = `Win: ${winCount} Loss: ${lossCount} Draw: ${drawCount}`;
+}
+
+resetButton.addEventListener("click", function () {
+  winCount = 0;
+  lossCount = 0;
+  drawCount = 0;
+  updateScore();
+  outputElement.textContent = "Start all over?";
 });
 
 // let finalOutput = winner("rock");
